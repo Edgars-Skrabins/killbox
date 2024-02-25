4757352c1dc523b3498cb6fa3168d7989d7b9b51
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-02-25 14:23:48 +0530

    Player Current Weapon Display Fixed

---
+++ Assets/Scripts/Utilities/AutoDisabler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDisabler : MonoBehaviour
+{
+    private float m_waitDuration = 3f;
+    private float m_fadeDuration = 2f;
+    [SerializeField] private CanvasGroup m_canvasGroup;
+
+    private void OnEnable()
+    {
+        m_canvasGroup.alpha = 1f;
+        StartCoroutine(StartFadeAnimation());
+    }
+
+    private IEnumerator StartFadeAnimation()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < m_fadeDuration)
+        {
+            m_canvasGroup.alpha = Mathf.Lerp(m_canvasGroup.alpha, 0, elapsedTime / m_fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        m_canvasGroup.alpha = 0f;
+        gameObject.SetActive(false);
+    }
+}
