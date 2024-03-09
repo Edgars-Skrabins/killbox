171dbebee709ccde0a379ee3663aaa38e8d2a687
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-03-10 01:06:08 +0530

    Added Custom Cursor on Button Hover
    Cleaned Button Animations Code + Added Custom Cursor on Button Hover

--- Assets/Scripts/Utilities/ButtonAnimVars.cs
+++ Assets/Scripts/Utilities/ButtonAnimVars.cs
@@ -1,7 +1,9 @@
+using UnityEngine;
+
 public static class ButtonAnimVars
 {
     public static float M_Duration = .25f;
 
     public static float M_UpScaleMultiplier = 1.2f;
-    public static float M_DownScaleMultiplier = 1.2f;
+    public static string M_CursorPath = "Assets/Resources/UI/Cursor.png";
 }
--- Assets/Scripts/Utilities/ButtonAnimation.cs
+++ Assets/Scripts/Utilities/ButtonAnimation.cs
@@ -7,23 +7,28 @@ public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler,IPointerExitH
     private Vector3 m_originalScale;
     private float m_duration = ButtonAnimVars.M_Duration;
     private float m_upScaleMultiplier = ButtonAnimVars.M_UpScaleMultiplier;
-    private float m_downScaleMultiplier = ButtonAnimVars.M_DownScaleMultiplier;
+
+    private Texture2D m_customCursor;
+    private string m_cursorTexturePath = ButtonAnimVars.M_CursorPath;
 
     private bool m_isHovering;
 
     void Start()
     {
+        SetCustomCursor(m_cursorTexturePath);
         m_originalScale = transform.localScale;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Cursor.SetCursor(m_customCursor, Vector2.zero, CursorMode.Auto);
         m_isHovering = true;
         StartCoroutine(ScaleAnimation());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         m_isHovering = false;
         if (gameObject.activeInHierarchy)
         {
@@ -33,6 +38,13 @@ public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler,IPointerExitH
             }
             StartCoroutine(ScaleAnimation());
         }
+    }
+
+    private void SetCustomCursor(string imagePath)
+    {
+        byte[] fileData = System.IO.File.ReadAllBytes(imagePath);
+        m_customCursor = new Texture2D(4, 4);
+        m_customCursor.LoadImage(fileData);
     }
 
     private IEnumerator ScaleAnimation()
