15f313add21e001706dcf4a9baef6646c758abbe
Edgars Skrabins <shiftkiro@gmail.com>
2024-03-09 12:39:32 +0200

    Changed condition of skipping splash screen

--- Assets/Scripts/Level/SplashScreenHandler.cs
+++ Assets/Scripts/Level/SplashScreenHandler.cs
@@ -32,7 +32,7 @@ public class SplashScreenHandler : MonoBehaviour
 
     private void Update()
     {
-        if (Input.anyKey)
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
         {
             SkipAnimation();
         }
