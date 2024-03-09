b61a1955cb1c9cd75f46975bc3b48c6a36d68947
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-03-09 05:55:26 +0530

    Added Low Graphics Mode

--- Assets/Scripts/Utilities/LowGraphics.cs
+++ Assets/Scripts/Utilities/LowGraphics.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
 
 public class LowGraphics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static string M_GRAPHICS_PREF = "GraphicsSettings";
+    private Toggle m_toggleLowGraphicsSettings;
+
+    [SerializeField] private RenderPipelineAsset[] m_qualityLevels;
+
+    private void Awake()
     {
-        
+        if (!m_toggleLowGraphicsSettings) m_toggleLowGraphicsSettings = GetComponent<Toggle>();
+
+        if (PlayerPrefs.HasKey(M_GRAPHICS_PREF))
+        {
+            m_toggleLowGraphicsSettings.isOn = PlayerPrefs.GetInt(M_GRAPHICS_PREF) == 0;
+            QualitySettings.SetQualityLevel(0);
+        }
+        else
+        {
+            m_toggleLowGraphicsSettings.isOn = true;
+            EnableHighGraphics(true);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+
+        m_toggleLowGraphicsSettings.onValueChanged.AddListener(EnableHighGraphics);
+    }
+
+    private void OnDisable()
+    {
+        m_toggleLowGraphicsSettings.onValueChanged.RemoveListener(EnableHighGraphics);
+    }
+
+    private void EnableHighGraphics(bool _isOn)
+    {
+        if (_isOn)
+        {
+            PlayerPrefs.SetInt(M_GRAPHICS_PREF, 0);
+            QualitySettings.SetQualityLevel(0);
+            QualitySettings.renderPipeline = m_qualityLevels[0];
+            return;
+        }
+        PlayerPrefs.SetInt(M_GRAPHICS_PREF, 1);
+        QualitySettings.SetQualityLevel(1);
+        QualitySettings.renderPipeline = m_qualityLevels[1];
     }
 }
