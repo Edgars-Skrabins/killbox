07253a6cb4bfdd3dbc3ee37d7657fdacf1f5f8ac
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-03-10 00:12:23 +0530

    Reduced Button Scaling Multiplier to 1.2x

--- Assets/Scripts/Utilities/ButtonAnimVars.cs
+++ Assets/Scripts/Utilities/ButtonAnimVars.cs
@@ -2,6 +2,6 @@ public static class ButtonAnimVars
 {
     public static float M_Duration = .25f;
 
-    public static float M_UpScaleMultiplier = 1.4f;
+    public static float M_UpScaleMultiplier = 1.2f;
     public static float M_DownScaleMultiplier = 1.2f;
 }
